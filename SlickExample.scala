e80c00162a0ff231c87d2d86d9f8acc1ef9d2c9e
Luc Perkins <lucperkins@gmail.com>
2013-09-10 11:20:48 -0700

    added a few new functions

--- SlickExample.scala
+++ SlickExample.scala
@@ -1,5 +1,3 @@
-package pdxscala
-
 // import scala.slick.lifted.BaseTypeMapper
 // import scala.slick.driver.BasicProfile
 import scala.slick.driver.PostgresDriver.simple._
@@ -9,23 +7,10 @@ import org.joda.time.DateTime
 import scala.util.{ Try, Success, Failure }
 import scala.slick.driver.PostgresDriver
 import com.github.tminglei.slickpg._
-import SlickPGSupport._
+// import SlickPGSupport._
 import com.github.tototoshi.csv._
 
 // import org.joda.time.format.ISODateTimeFormat
-
-object CSV {
-	import java.io.File
-	import scala.collection.mutable.ListBuffer
-
-	def convertCSVToTweets(filename: String) = {
-		val reader = CSVReader.open(new File(filename))
-		val rawList = reader.iterator.toList
-		val tweets = new ListBuffer[(Long, String)]
-		rawList.foreach(line => tweets ++= List((line(0).toLong, line(1))))
-		tweets.toList
-	}
-}
 
 object PostgresSupport {
 	val db = Database.forURL(
@@ -39,12 +24,12 @@ case class Tweet(tweetId:      Long,
 	             lastModified: DateTime,
 	             content:      String,
 	             retweeted:    Boolean,
-	             userId:       Long)
+	             username:     String)
 	
 case class User(userId:   Long,
 	            username: String)
 
-object TweetDAO extends SlickPGSupport {
+object TweetDAO /*extends SlickPGSupport*/ {
 	import CSV._
 	import PostgresSupport._
 
@@ -54,40 +39,37 @@ object TweetDAO extends SlickPGSupport {
 		def lastModified = column[DateTime] ("lastmodified", O.DBType("TIMESTAMP"))
 		def content      = column[String]   ("content", O.DBType("VARCHAR(140)"))
 		def retweeted    = column[Boolean]  ("retweeted", O.DBType("BOOLEAN"))
-		def userId       = column[Long]     ("userId", O.DBType("BIGINT"))
+		def username     = column[String]   ("username", O.DBType("VARCHAR(10)"))
 
-		def fUserId      = foreignKey("ID_FK", userId, Users)(_.userId)
-		def tweetIdx     = index("TWEET_INDEX", on = tweetId, unique = true)
-
-		def *            = (tweetId ~ created ~ lastModified ~ content ~ retweeted ~ userId) <> (Tweet, Tweet.unapply _)
-		def forInsert    = (created ~ lastModified ~ content ~ retweeted ~ userId) returning tweetId
+		def *            = (tweetId ~ created ~ lastModified ~ content ~ retweeted ~ username) <> (Tweet, Tweet.unapply _)
+		def forInsert    = (created ~ lastModified ~ content ~ retweeted ~ username) returning tweetId
 		def findById     = createFinderBy(_.tweetId)
-
+        def findByCr     = createFinderBy(_.created)
+        def findByLast   = createFinderBy(_.lastModified)
 	}
 
-	object Users extends Table[User]("users") {
+	/*object Users extends Table[User]("users") {
 		def userId   = column[Long]   ("userId", O.AutoInc, O.PrimaryKey, O.DBType("BIGINT"))
 		def username = column[String] ("username", O.DBType("VARCHAR(10)"))
-		// def tweets   = column[List[Long]] ("tweets", O.DBType("BIGINT[]"))
 		def *        = (userId ~ username) <> (User, User.unapply _)
 		def autoInc  = username returning userId
-	}
+	}*/
 
 	lazy val allTweets: List[Tweet] = db.withSession {
 		Query(Tweets).list
 	}
 
-	def addMultipleTweets(args: List[(Long, String)]) = {
+	def addMultipleTweets(args: List[(String, String)]) = {
 		Try(args.map(arg => addTweet(arg._1, arg._2))) match {
 			case Success(_) => println("Tweets have been successfully added")
 			case Failure(_) => println("Something went wrong")
 		}
 	}
 
-	def addTweet(userId: Long, content: String) = {
+	def addTweet(username: String, content: String) = {
 		db.withSession {
 			val now = new DateTime()
-			Try (Tweets.forInsert.insert(now, now, content, false, userId)) match {
+			Try (Tweets.forInsert.insert(now, now, content, false, username)) match {
 				case Success(_) => println("Tweet has been successfully added")
 				case Failure(_) => println("Something went wrong")
 			}
@@ -100,14 +82,25 @@ object TweetDAO extends SlickPGSupport {
 		}
 	}
 
-	/*def deleteTweetById(tweetId: Long) = {
+    def unionQueryExample = db.withSession {
+        ((Query(Tweets).filter(_.content > "b")) union (Query(Tweets).filter(_.content < "m"))).list
+    }
+
+    val tweetByIdRange = for {
+        (min, max) <- Parameters[(Long, Long)]
+        t <- Tweets if t.tweetId > min && t.tweetId < max
+    } yield t
+
+    /*
+    def deleteTweetById(id: Long) = {
 		db.withSession {
-			Try (Tweets.where(_.tweetId is tweetId).delete) match {
-				case Success(_) => println("Tweet was successfully deleted")
-				case Failure(_) => println("Something went wrong")
+			Tweets.filter(_.tweetId === id).delete match {
+				case 1 => println("Tweet was successfully deleted")
+				case _ => println("Something went wrong")
 			}
 		}
-	}*/
+	}
+    */
 
 	def modifyTweetById(tweetId: Long) = {
 		db.withSession {
@@ -120,6 +113,14 @@ object TweetDAO extends SlickPGSupport {
 			}
 		}
 	}
+
+    /* 
+    def fetchTweetByUser(id: Long) = {
+        db.withSession {
+            Tweets.where(_.tweetId === userId).list
+        }
+    }
+    */
 
 	def findByMultipleIds(ids: Long*) = {
 		db.withSession {
@@ -169,18 +170,18 @@ object TweetDAO extends SlickPGSupport {
 		}
 	}
 
-	def bigJoin(id: Long) = db.withSession {
+	/*
+    def bigJoin(id: Long) = db.withSession {
 		(for {
 			tweet <- Tweets if tweet.tweetId === id
 			user <- Users if user.userId === id
 		} yield tweet.content).list
 	}
+    */
 
 	def createTables = db.withSession {
-		Try {
-			Users.ddl.create
-			Tweets.ddl.create
-		} match {
+        val ddl = Tweets.ddl
+		Try (ddl.create) match {
 			case Success(_) => println("Tables successfully created")
 			case Failure(e) => println(e.getMessage)
 		}
@@ -188,24 +189,33 @@ object TweetDAO extends SlickPGSupport {
 
 	def dropTables = {
 		db.withSession {
-			Try {
-				Users.ddl.drop
-				Tweets.ddl.drop
-			} match {
+			Try (Tweets.ddl.drop) match {
 				case Success(_) => println("Tables successfully dropped")
 				case Failure(e) => println(e.getMessage)
 			}
 		}
 	}
 
-	def populateDB(filename: String) = {
-		addMultipleTweets(CSV.convertCSVToTweets(filename))
-	}
-
 	def showSQL     = {
 		Tweets.ddl.createStatements.foreach(println(_))
-		Users.ddl.createStatements.foreach(println(_))
 	}
+
+    def populateDB(filename: String) = {
+        addMultipleTweets(CSV.convertCSVToTweets(filename))
+    }
+}
+
+object CSV {
+    import java.io.File
+    import scala.collection.mutable.ListBuffer
+
+    def convertCSVToTweets(filename: String) = {
+        val reader = CSVReader.open(new File(filename))
+        val rawList = reader.iterator.toList
+        val tweets = new ListBuffer[(String, String)]
+        rawList.foreach(line => tweets ++= List((line(0), line(1))))
+        tweets.toList
+    }
 }
 
 /*
@@ -217,18 +227,9 @@ import scala.slick.driver.PostgresDriver.simple._
 import Database.threadLocalSession
 import com.github.tototoshi.slick.JodaSupport._
 import org.joda.time.DateTime
-val db = pdxscala.PostgresSupport.db
-import pdxscala.TweetDAO
-import pdxscala.TweetDAO.{ Tweets, Users }
-import pdxscala.CSV._
-
-db.withSession {
-	Users.ddl.create
-	Tweets.ddl.create
-}
-
-Users.autoInc.insert(
-	List("jane", "tarzan", "linus", "")
-)
+val db = PostgresSupport.db
+import TweetDAO._
+import TweetDAO.Tweets
+import CSV._
 
 */
